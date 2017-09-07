5ea9facae5d7a474ca764dc51873286a461d06f9
murabye <vlada_kuz2011@mail.ru>
2017-09-07 14:17:22 +0500

    решение через расстояние между точками и прямой

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,57 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MyLib;
-using static System.Math;
+using System.IO;
 
 namespace ex2
 {
     internal class Program
     {
-        private static double ToRad(double grad)
-        {
-            return grad * Math.PI / 180;
-        }
 
         private static void Main()
         {
-            while (true)
+            double radius; // радиус, от 100 до 10 000 
+            double lat1, lat2, long1, long2;
+
+            var reader = new FileInOut("input");
+
+            // инициализация
+            radius = Double.Parse(reader.Next());
+            lat1 =  Double.Parse(reader.Next());
+            long1 = Double.Parse(reader.Next());
+            lat2 =  Double.Parse(reader.Next());
+            long2 = Double.Parse(reader.Next());
+
+            // перевод в радианы
+            double x1 = radius * Math.Cos(lat1) * Math.Cos(long1);
+            double x2 = radius * Math.Cos(lat2) * Math.Cos(long2);
+            double y1 = radius * Math.Cos(lat1) * Math.Sin(long1);
+            double y2 = radius * Math.Cos(lat2) * Math.Sin(long2);
+            double z1 = radius * Math.Sin(lat1);
+            double z2 = radius * Math.Sin(lat2);
+
+            // поиск расстояния между точками по прямой
+            double D = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+            if (D > 2 * radius) D = 2 * radius;
+
+            double tangleA = 2 * Math.Asin((D / 2) / radius);
+            FileInOut.ToFile("output", tangleA.ToString("#.##").Replace(',', '.'));
+        }
+
+        // класс для считывания и записи в файл
+        internal class FileInOut
+    {
+        private string[] _obj;                      // строки, которые нужно считать
+        private int position = 0;                   // позиция считывателя
+
+        /* конструктор для ввода из файла
+         * name - название файла
+         * НЕ ПОДДЕРЖИВАЕТСЯ выбор пути, все в папку исходную
+         */
+        public FileInOut(string name)
+        {
+            var _reader = new StreamReader(name + ".txt");
+            var strings = _reader.ReadToEnd();
+            _obj = strings.Split('\n', ' ');
+
+            _reader.Close();
+        }
+
+        // возрващает считываемую строку, делит по пробелам
+        public string Next()
+        {
+            try
             {
-                double radius; // радиус, от 100 до 10 000 
-                double lat1, lat2, long1, long2;
-
-                // инициализация
-                radius = Ask.RealNum("Введите радиус: ", 10, 10000);
-                lat1 = Ask.RealNum("Введите широту первой точки: ", -90, 90);
-                long1 = Ask.RealNum("Введите долготу первой точки: ", -180, 180);
-                lat2 = Ask.RealNum("Введите широту второй точки: ", -90, 90);
-                long2 = Ask.RealNum("Введите долготу второй точки: ", -180, 180);
-
-                // перевод в радианы
-                lat1 = ToRad(lat1);
-                lat2 = ToRad(lat2);
-                long1 = ToRad(long1);
-                long2 = ToRad(long2);
-
-                // синусы и косинусы для вычисления
-                double x, y;
-
-                // вычисления
-                // формула гаверсинусов с модификацией для антиподов
-                y = Sqrt(Pow(Cos(lat2) * Sin(long2 - long1), 2) 
-                    + Pow(Cos(lat1) * Sin(lat2)
-                    - Sin(lat1) * Cos(lat2) * Cos(long2 - long1), 2));
-
-                x = Sin(lat1) * Sin(lat2)
-                    + Cos(lat1) * Cos(lat2) * Cos(long2 - long1);
-                
-                double ans = Atan2(y, x) * radius;
-                
-                Console.WriteLine("\nОтвет: " + ans);
-                OC.Stay();
+                return _obj[position++];
+            }
+            catch (Exception e)
+            {
+                return " ";
             }
         }
+
+        /* вывод в файл name строки obj
+         * name вводится без расширения
+         * НЕ ПОДДЕРЖИВАЕТСЯ выбор пути, все в папку исходную
+         */
+        public static void ToFile(string name, string obj)
+        {
+            var f = new StreamWriter(name + ".txt");
+            var strings = obj.Split('\n');
+
+            foreach (var t in strings)
+                f.WriteLine(t);
+
+            f.Close();
+        }
+
     }
 }
